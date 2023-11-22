7c84c50adee31a6c34437fe775940bf524a0d0f7
cakeinmilk <barrydonovan@gmail.com>
2023-11-22 23:13:43 +0000

    .

--- js/sortArticles.js
+++ js/sortArticles.js
@@ -1,11 +1,11 @@
 function sortTable(columnIndex) {
-    let rows = Array.from(document.querySelectorAll('.table-row'));
+    let rows = Array.from(document.querySelectorAll('.art-table-row'));
     let sortedRows = rows.sort((a, b) => {
         let aValue = a.children[columnIndex].textContent;
         let bValue = b.children[columnIndex].textContent;
         // Adjust comparison for date or other data types as needed
         return aValue.localeCompare(bValue);
     });
-    let table = document.querySelector('.table');
+    let table = document.querySelector('.art-table');
     sortedRows.forEach(row => table.appendChild(row));
 }
