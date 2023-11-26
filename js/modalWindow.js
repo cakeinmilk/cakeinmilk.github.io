485ca0c568a93641942d39b25f10946264356e72
cakeinmilk <barrydonovan@gmail.com>
2023-11-26 10:38:53 +0000

    Update modalWindow.js

--- js/modalWindow.js
+++ js/modalWindow.js
@@ -78,7 +78,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     if (fileExtension === 'pdf') {
       // Handle PDFs
-      content = `<iframe src="${fileUrl}" style="max-width:100%;height:auto;"></iframe>`;
+      content = `<iframe src="${fileUrl}" style="width:100%; height:80vh;" frameborder="0"></iframe>`;
     } else if (fileExtension === 'jpg' || fileExtension === 'jpeg') {
       // Handle JPG images
       content = `<img src="${fileUrl}" style="max-width:100%;height:auto;">`;
